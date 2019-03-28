4443cb6af06a475db5c3d402fbdfd68207a62740
aztek <evgeny.kotelnikov@gmail.com>
2019-03-28 12:44:35 +0100

    Implement more shrink functions

--- test/QuickCheckSpec/Generators.hs
+++ test/QuickCheckSpec/Generators.hs
@@ -9,6 +9,7 @@ import GHC.Generics
 import Generic.Random
 import Test.QuickCheck hiding (Sorted, Function)
 
+import Data.Bitraversable (bitraverse)
 import Data.Char (chr)
 import Data.List.NonEmpty (NonEmpty(..))
 import Data.Text (pack)
@@ -23,6 +24,11 @@ instance Arbitrary s => Arbitrary (NonEmpty s) where
 deriving instance Generic (Name s)
 instance Arbitrary s => Arbitrary (Name s) where
   arbitrary = genericArbitraryU
+
+shrinkMaybe :: (a -> [a]) -> Maybe a -> [Maybe a]
+shrinkMaybe s = \case
+  Nothing -> []
+  Just a  -> Nothing : fmap Just (s a)
 
 alphaNumeric :: String
 alphaNumeric = '_' : ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']
@@ -76,9 +82,7 @@ instance Arbitrary Literal where
   arbitrary = genericArbitraryU
   shrink = \case
     Predicate p ts -> Predicate p <$> shrinkList shrink ts
-    Equality a s b -> do a' <- shrink a
-                         b' <- shrink b
-                         return (Equality a' s b')
+    Equality a s b -> Equality <$> shrink a <*> pure s <*> shrink b
     _ -> []
 
 deriving instance Generic Sign
@@ -88,7 +92,7 @@ instance Arbitrary Sign where
 deriving instance Generic Clause
 instance Arbitrary Clause where
   arbitrary = genericArbitraryU
-  -- shrink (Clause ls) = Clause <$> shrinkList shrink ls
+  shrink (Clause ls) = Clause <$> shrink ls
 
 deriving instance Generic Quantifier
 instance Arbitrary Quantifier where
@@ -105,20 +109,16 @@ instance Arbitrary Unsorted where
 deriving instance Generic Sorted
 instance Arbitrary Sorted where
   arbitrary = genericArbitraryU
-  shrink = \case
-    Sorted (Just _) -> [Sorted Nothing]
-    _ -> []
+  shrink (Sorted s) = Sorted <$> shrinkMaybe shrink s
 
 deriving instance Generic (FirstOrder s)
 instance Arbitrary s => Arbitrary (FirstOrder s) where
   arbitrary = genericArbitraryRec (3 % 2 % 2 % 1 % ())
   shrink = \case
     Atomic l -> Atomic <$> shrink l
-    Quantified q vs f -> f : (Quantified q vs <$> shrink f)
     Negated f -> f : (Negated <$> shrink f)
-    Connected f c g -> [f, g] ++ do f' <- shrink f
-                                    g' <- shrink g
-                                    return (Connected f' c g')
+    Quantified q vs f -> f : (Quantified q vs <$> shrink f)
+    Connected f c g -> f : g : (Connected <$> shrink f <*> pure c <*> shrink g)
 
 deriving instance Generic Formula
 instance Arbitrary Formula where
@@ -142,9 +142,9 @@ deriving instance Generic Source
 instance Arbitrary Source where
   arbitrary = genericArbitraryRec (2 % 2 % 2 % 2 % 2 % 2 % 2 % 1 % ())
   shrink = \case
-    Theory  n (Just _) -> [Theory  n Nothing]
-    Creator n (Just _) -> [Creator n Nothing]
-    Introduced i (Just _) -> [Introduced i Nothing]
+    Theory  n info -> Theory  n <$> shrinkMaybe shrink info
+    Creator n info -> Creator n <$> shrinkMaybe shrink info
+    Introduced i info -> Introduced i <$> shrinkMaybe shrink info
     Inference n i ps -> Inference n <$> shrink i <*> shrinkList shrink ps
     Sources ss -> Sources <$> shrink ss
     _ -> []
@@ -152,25 +152,23 @@ instance Arbitrary Source where
 deriving instance Generic Parent
 instance Arbitrary Parent where
   arbitrary = genericArbitraryRec (1 % ())
-  shrink (Parent s gts) = Parent s <$> shrinkList shrink gts
+  shrink (Parent s gts) = Parent <$> shrink s <*> shrinkList shrink gts
 
 deriving instance Generic GeneralData
 instance Arbitrary GeneralData where
   arbitrary = genericArbitraryRec (1 % 2 % 2 % 2 % 2 % ())
   shrink = \case
     GeneralFunction f gts -> GeneralFunction f <$> shrinkList shrink gts
-    GeneralVariable _     -> []
-    GeneralNumber _       -> []
     GeneralFormula f      -> GeneralFormula <$> shrink f
     GeneralBind v f       -> GeneralBind v <$> shrink f
+    _ -> []
 
 deriving instance Generic GeneralTerm
 instance Arbitrary GeneralTerm where
   arbitrary = genericArbitraryRec (1 % 1 % ())
   shrink = \case
-    GeneralData gd Nothing   -> GeneralData <$> shrink gd <*> return Nothing
-    GeneralData gd (Just gt) -> GeneralData <$> shrink gd <*> fmap Just (shrink gt)
-    GeneralList gts          -> GeneralList <$> shrinkList shrink gts
+    GeneralData gd gt -> GeneralData <$> shrink gd <*> shrinkMaybe shrink gt
+    GeneralList gts   -> GeneralList <$> shrinkList shrink gts
 
 deriving instance Generic Info
 instance Arbitrary Info where
@@ -182,7 +180,9 @@ instance Arbitrary Info where
 deriving instance Generic Unit
 instance Arbitrary Unit where
   arbitrary = genericArbitraryRec (1 % ())
-  shrink (Unit nm r f ann) = do { f' <- shrink f; return (Unit nm r f' ann) }
+  shrink (Unit nm r f ann) = Unit nm r <$> shrink f <*> shrinkAnn ann
+    where
+      shrinkAnn = shrinkMaybe $ bitraverse shrink (shrinkMaybe shrink)
 
 deriving instance Generic Derivation
 instance Arbitrary Derivation where
